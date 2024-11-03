144cdbf153897e7ee070deb5d05fd5068fab0390
Rushil Talla <rushilt@umich.edu>
2024-11-03 15:38:43 -0500

    Add recipes

--- softwaredev_pages/Cookbook/generator.py
+++ softwaredev_pages/Cookbook/generator.py
@@ -7,7 +7,7 @@ import shutil
 GENERATED = "./Recipes/"
 DATA = "./data/"
 
-
+# Generate one recipe
 def gen_recipe(file_path):
     base_name = os.path.splitext(os.path.basename(file_path))[0] 
 
@@ -21,6 +21,7 @@ def gen_recipe(file_path):
     # Render the template with data from the JSON file
     output = template.render(
         title=base_name.replace("_", " ").title(),
+        equipment=data['equipment'],
         ingredients=data['ingredients'],
         steps=data['steps']
     )
@@ -29,12 +30,25 @@ def gen_recipe(file_path):
     with open(new_filename, 'w') as output_file:
         output_file.write(output)
 
-def gen_main():
-    # Generate main file
-    print("Gen Main")
+# Generate main file
+def gen_main(files):
+    base_names = [f.replace(".json", "") for f in files]
+
+    # Set up Jinja2 environment and load the template
+    env = Environment(loader=FileSystemLoader('./templates'))
+    template = env.get_template('main.tmpl')
+
+    # Render the template with data from the JSON file
+    output = template.render(
+        title=[f.replace("_", " ").title() for f in base_names],
+        names=base_names
+    )
+
+    new_filename = './main.html'
+    with open(new_filename, 'w') as output_file:
+        output_file.write(output)
 
 def read_recipes(folder_path):
-    # List of file names being processed
     # Check each file in folder
     for file in os.listdir(folder_path):
         print(f"- Generating recipe: {file}")
@@ -42,7 +56,7 @@ def read_recipes(folder_path):
 
         gen_recipe(file_path)
 
-    gen_main()
+    gen_main(os.listdir(folder_path))
 
 
 if __name__ == "__main__":
